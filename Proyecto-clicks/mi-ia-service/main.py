c271652e78f4ed579b9e7df9a2aefa9e8a125208
FreddyGUIDE <163678893+FredyNotAvailable@users.noreply.github.com>
2025-07-10 17:23:19 -0500

    Nuvo

--- Proyecto-clicks/mi-ia-service/main.py
+++ Proyecto-clicks/mi-ia-service/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from model import ClickPredictor
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://click-app-ai.onrender.com"],  # Cambia "*" por ["https://tu-frontend.com"] cuando estés en producción
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 predictor = ClickPredictor()
 
 class PredictRequest(BaseModel):
